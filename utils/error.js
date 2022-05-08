f3a27c46c0060d5dcda56dfde8260130a663e512
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2022-05-08 08:30:52 +0700

    fix import error

--- utils/error.js
+++ utils/error.js
@@ -9,4 +9,4 @@ const onError = async (err, req, res, next) => {
   await db.disconnect();
   res.status(500).send({ message: err.toString() });
 };
-export { getError, onError };
+export default { getError, onError };
